9aa06f08b07915224125cf6608b203ffc783adfb
yashikanpatel <yashikapatel0709@gmail.com>
2026-08-18 01:47:36 +0530

    Time: 1 ms (77.40%) | Memory: 46.2 MB (97.81%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
@@ -1,5 +1,6 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
+
         // PATTERN: Two Pointers
         // i = position of last unique element
         // j = scans the array
@@ -17,3 +18,23 @@ class Solution {
         
     }
 }
+/* // PATTERN: Boyer-Moore Voting Algorithm
+        // Different elements cancel each other.
+        
+        int candidate = nums[0];
+        int count = 0;
+
+        for (int num : nums) {
+
+            if (count == 0) {
+                candidate = num;
+            }
+
+            if (num == candidate) {
+                count++;
+            } else {
+                count--;
+            }
+        }
+
+        return candidate; */
