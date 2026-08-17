c9d7abeb60f8531b90b9dc258a1b8c4c38071099
yashikanpatel <yashikapatel0709@gmail.com>
2026-08-18 01:30:59 +0530

    Time: 2 ms (92.04%) | Memory: 47.6 MB (88.92%) - LeetSync

---
+++ 283-move-zeroes/move-zeroes.java
@@ -0,0 +1,19 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+                // PATTERN: Two Pointers
+        int j = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+
+            if (nums[i] != 0) {
+
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+
+                j++;
+            }
+        }
+        
+    }
+}
